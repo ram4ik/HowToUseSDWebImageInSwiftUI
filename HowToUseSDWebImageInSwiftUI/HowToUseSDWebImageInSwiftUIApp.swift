2e2163fcd6c6660d755332bf3b580b912501c7fc
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-03-12 21:38:26 +0200

    Initial Commit

---
+++ HowToUseSDWebImageInSwiftUI/HowToUseSDWebImageInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseSDWebImageInSwiftUIApp.swift
+//  HowToUseSDWebImageInSwiftUI
+//
+//  Created by test on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseSDWebImageInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
